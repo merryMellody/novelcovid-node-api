6b6d6dffb855060d245c00c77e60601195abcd85
Meredith Mello <merry@mello.codes>
2020-03-26 00:55:09 -0700

    General code cleanup, added gitignore/package-lock

--- index.js
+++ index.js
@@ -1,28 +1,12 @@
-/*
-  Enjoy the new functions I have created! More coming soon if diced or elite makes useful updates
-  - Apollo
-*/
+const fetch = require("node-fetch");
+
+const BASE_URL = "https://corona.lmao.ninja";
+
 module.exports = {
-  all: async function() {
-    // Wait why did you .toString() this function?
-    const a = await require('node-fetch')(`https://corona.lmao.ninja/all`);return await a.json();
-  },
-  countries: async function(country) {
-    // Wait why did you .toString() this function?
-    if (!country) {
-         const a = await require('node-fetch')('https://corona.lmao.ninja/countries'); return await a.json();
-    } else if (country) {
-       try { const a = await require('node-fetch')(`https://corona.lmao.ninja/countries/${country}`); return await a.json();
-       } catch (e) { return { error: 'Country not found' } }
-    }
-  },
-  states: async function(state) {
-    // Wait why did you .toString() this function?
-    if (!state) { 
-       const a = await require('node-fetch')('https://corona.lmao.ninja/states');
-       return await a.json();
-    }
-    // Coming Soon VVV
-    // else if (state) try { const a = await require('node-fetch')(`https://corona.lmao.ninja/states/${state}`); return await a.json(); } catch(e) { return { error: 'State not found' } }
-  }
-}
+  all: () => fetch(`${BASE_URL}/all`).then(a => a.json()),
+  countries: (country = "") =>
+    fetch(`${BASE_URL}/countries/${country}`)
+      .then(a => a.json())
+      .catch(() => ({ error: "Country not found" })),
+  states: () => fetch(`${BASE_URL}/states`).then(a => a.json())
+};
